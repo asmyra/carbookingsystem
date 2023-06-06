0b1e0224171292e79b8dc363072410fad4aa3dd7
asmyra <nur.asmyra@yahoo.com>
2023-06-06 11:27:16 +0800

    summary

---
+++ admin/createnotifaction.php
@@ -0,0 +1,53 @@
+<?php
+
+// This PHP code sends a notification to a user in the system for a vehicle booking status.
+
+// Import the necessary libraries.
+require_once('vendor/autoload.php');
+
+// Connect to the database.
+$db = new PDO('mysql:host=localhost;dbname=vehicle_booking', 'root', '');
+
+// Get the booking status.
+$sql = 'SELECT status FROM bookings WHERE id = :id';
+$stmt = $db->prepare($sql);
+$stmt->bindParam(':id', $id);
+$stmt->execute();
+$status = $stmt->fetchColumn();
+
+// Get the user information.
+$sql = 'SELECT name, email FROM users WHERE id = :id';
+$stmt = $db->prepare($sql);
+$stmt->bindParam(':id', $user_id);
+$stmt->execute();
+$user = $stmt->fetch();
+
+// Send the notification.
+$subject = 'Vehicle Booking Status';
+$message = 'Your vehicle booking has been updated to status: ' . $status;
+
+// Send the notification to the user.
+$notification = new \stdClass();
+$notification->subject = $subject;
+$notification->message = $message;
+$notification->user = $user;
+
+$mailer = new \PHPMailer\PHPMailer();
+$mailer->isSMTP();
+$mailer->Host = 'smtp.example.com';
+$mailer->SMTPAuth = true;
+$mailer->Username = 'user@example.com';
+$mailer->Password = 'password';
+$mailer->setFrom('user@example.com', 'John Doe');
+$mailer->addAddress($user->email, $user->name);
+$mailer->Subject = $notification->subject;
+$mailer->Body = $notification->message;
+
+if ($mailer->send()) {
+  // The notification was sent successfully.
+} else {
+  // The notification was not sent successfully.
+  echo $mailer->ErrorInfo;
+}
+
+?>
---
+++ admin/generatereport.php
@@ -0,0 +1,36 @@
+<?php
+
+// This PHP code generates a yearly and monthly report of vehicle usage in a company in PDF format.
+
+// Import the necessary libraries.
+require_once('vendor/autoload.php');
+
+// Create a new PDF document.
+$pdf = new \TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
+
+// Set the document information.
+$pdf->SetCreator('Company Name');
+$pdf->SetAuthor('John Doe');
+$pdf->SetTitle('Vehicle Usage Report');
+$pdf->SetSubject('This report shows the yearly and monthly usage of vehicles in the company.');
+
+// Set the header and footer.
+$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, 'Vehicle Usage Report', 'Generated on ' . date('Y-m-d'));
+$pdf->SetFooterData(array(0, 0, 0), array(0, 0, 0), 'Page ' . $pdf->getPageNo() . '/' . $pdf->getPages());
+
+// Add some content to the document.
+$pdf->AddPage();
+$pdf->Write(5, 'This report shows the yearly and monthly usage of vehicles in the company.');
+$pdf->Write(5, 'The following table shows the yearly usage of vehicles in the company:');
+$pdf->WriteTable(array(
+  'Year', 'Total Miles Driven', 'Average Miles Driven per Month'
+), $data);
+$pdf->Write(5, 'The following table shows the monthly usage of vehicles in the company:');
+$pdf->WriteTable(array(
+  'Month', 'Total Miles Driven', 'Average Miles Driven per Day'
+), $data);
+
+// Close the document.
+$pdf->Output('vehicle_usage_report.pdf', 'I');
+
+?>
